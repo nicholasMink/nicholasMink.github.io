b10b18b1d683c0c17514713820e442b17b1d68e0
Nick <nicholasmink@outlook.com>
2017-12-07 20:06:20 -0600

    adding transitions

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // index.js
 
+// window
+let fullScreen = document.querySelector('.fullView');
+
 // buttons
 let prj = document.getElementById('projects');
 let codePen = document.getElementById('cpe');
@@ -16,41 +19,50 @@ let slackDisplay = document.querySelector('.slack');
 
 
 codePen.addEventListener('click', function switchToCodepen() {
-  prjDisplay.style.display = "none";
-  gitDisplay.style.display = "none";
-  linkedInDisplay.style.display = "none";
-  slackDisplay.style.display = "none";
+  hideAll();
+  cpeDisplay.style.opacity = 0;
   cpeDisplay.style.display = "block";
+  freshView(cpeDisplay);
 });
 
 prj.addEventListener('click', function switchToProjects() {
-  cpeDisplay.style.display = "none";
-  gitDisplay.style.display = "none";
-  linkedInDisplay.style.display = "none";
-  slackDisplay.style.display = "none";
+  hideAll();
+  prjDisplay.style.opacity = 0;
   prjDisplay.style.display = "block";
+  freshView(prjDisplay);
 });
 
 github.addEventListener('click', function switchToProjects() {
-  cpeDisplay.style.display = "none";
-  prjDisplay.style.display = "none";
-  linkedInDisplay.style.display = "none";
-  slackDisplay.style.display = "none";
+  hideAll();
+  gitDisplay.style.opacity = 0;
   gitDisplay.style.display = "block";
+  freshView(gitDisplay);
 });
 
 linkedIn.addEventListener('click', function switchToProjects() {
-  cpeDisplay.style.display = "none";
-  prjDisplay.style.display = "none";
-  slackDisplay.style.display = "none";
-  gitDisplay.style.display = "none";
+  hideAll();
+  linkedInDisplay.style.opacity = 0;
   linkedInDisplay.style.display = "block";
+  freshView(linkedInDisplay);
 });
 
 slack.addEventListener('click', function switchToProjects() {
+  hideAll();
+  slackDisplay.style.opacity = 0;
+  slackDisplay.style.display = "block";
+  freshView(slackDisplay);
+});
+
+function hideAll( ) {
+  TweenMax.to(".recent, .codepen, .github, .linkedIn, .slack", .5, {opacity:0});
   cpeDisplay.style.display = "none";
   prjDisplay.style.display = "none";
-  gitDisplay.style.display = "none";
   linkedInDisplay.style.display = "none";
-  slackDisplay.style.display = "block";
-});
+  slackDisplay.style.display = "none";
+  gitDisplay.style.display = "none";
+}
+
+function freshView( specialSection ) {
+  TweenMax.to(".fullView", 1, {background:"background: url('./img/bg-pattern.svg')"});
+  TweenMax.to(specialSection, 1, {opacity:1});
+}
