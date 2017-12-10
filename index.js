931b0893d23de1e09cc0e459f6c1b401e8e0656b
Nick <nicholasmink@outlook.com>
2017-12-10 10:13:52 -0600

    removing service worker from index

--- index.js
+++ index.js
@@ -71,16 +71,16 @@ function freshView( specialSection ) {
 // SERVICE WORKER
 // ______________________________________________________________________________________________
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('./service-worker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function() {
+//     navigator.serviceWorker.register('./service-worker.js').then(function(registration) {
+//       // Registration was successful
+//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//     }, function(err) {
+//       // registration failed :(
+//       console.log('ServiceWorker registration failed: ', err);
+//     });
+//   });
+// }
 // ______________________________________________________________________________________________
 
