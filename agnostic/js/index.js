aab6fc7cd867fdf83f2d5d543852aff38d2016e8
Nick <nicholasmink@outlook.com>
2018-07-30 16:58:40 -0500

    fix: Add check for undefined coordinates

--- agnostic/js/index.js
+++ agnostic/js/index.js
@@ -17,10 +17,45 @@ $.ajax({
     mapControl(data);
 });
 
-console.log(dogs);
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 30.2972, lng: -97.7431},
+    zoom: 10
+  });
+}
+
+const setMapMarkers = (lat, long, markerTip, first, last) => {
+  const content = `<h3>${markerTip}</h3><h4>Owner - ${first} ${last}</h4>`;
+  var infowindow = new google.maps.InfoWindow({
+    content: content
+  });
+
+  var latLng = new google.maps.LatLng(lat, long);
+  var marker = new google.maps.Marker({
+    position: latLng,
+    map: map,
+    title: content,
+    animation: google.maps.Animation.DROP
+  });
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+
+}
+
+// console.log(dogs);
 let mapControl = (mapData) => {
+  // console.table(mapData)
   mapData.forEach(dog => {
-    setMapMarkers(dog.location.coordinates[1], dog.location.coordinates[0], dog.description_of_dog, dog.first_name, dog.last_name);
+    if (dog.location) {
+      let lat = dog.location.coordinates[1];
+      let long = dog.location.coordinates[0];
+      let description = dog.description_of_dog;
+      let ownerFirst = dog.first_name;
+      let ownerLast = dog.last_name;
+      setMapMarkers(lat, long, description, ownerFirst, ownerLast);
+    }
   });
 }
 
@@ -66,30 +101,3 @@ const createDogElements = (data) => {
 
 // console.log('dogData:', dogData);
 
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 30.2972, lng: -97.7431},
-    zoom: 10
-  });
-}
-
-const setMapMarkers = (lat, long, markerTip, first, last) => {
-  const content = `<h3>${markerTip}</h3><h4>Owner - ${first} ${last}</h4>`;
-  var infowindow = new google.maps.InfoWindow({
-    content: content
-  });
-
-  var latLng = new google.maps.LatLng(lat, long);
-  var marker = new google.maps.Marker({
-    position: latLng,
-    map: map,
-    title: content,
-    animation: google.maps.Animation.DROP
-  });
-
-  marker.addListener('click', function() {
-    infowindow.open(map, marker);
-  });
-
-}
